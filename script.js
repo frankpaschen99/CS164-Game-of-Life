ae060becdd8fab87e0a271bcfd0264de347e60b6
Frank Paschen <fhp23@cs.drexel.edu>
2018-11-12 13:22:34 -0500

    ok no it doesnt actually work

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-// TODO 11/12/18:
 /*
-	fix undefined error when trying to find neighbors
-	of border tiles
+	This doesnt work properly. Gotta be an issue
+	counting live neighbors because the rules look
+	good.
+	
+	will fix later
+
 */
 
 var board;
@@ -14,9 +17,7 @@ var speed = 1000;	// adjustable (ms)
 var scenario = 2;	// adjustable - todo: implement some scenarios from wikipedia page
 
 function init() {
-	console.log("hello");
 	board = new Array(boardSize);
-
 	for (var i = 0; i < boardSize; i++) {
 		board[i] = new Array(boardSize);
 		for (var j = 0; j < boardSize; j++) {
@@ -28,7 +29,6 @@ function init() {
 	ctx = canvas.getContext('2d');
 	tileSize = ctx.canvas.width / boardSize;
 	
-	
 	if (scenario == 1) {	// random
 		for (var row = 25; row < 30; row++) {
 			for (var col = 25; col < 30; col++) {
@@ -36,10 +36,12 @@ function init() {
 			}
 		}
 	} else if (scenario == 2) { // 10 cell row
-		for (var col = 25; col < 36; col++) {
-			board[col][col] = 1;
+		for (var row = 25; row < 36; row++) {
+			board[25][row] = 1;
 		}
 	}
+	
+	render();
 	setInterval(step, speed);
 }
 var backup;
@@ -71,8 +73,8 @@ function step() {
 			if (tileState == 1 && liveNeighbors < 2) {
 				backup[col][row] = 0;
 			} else if (tileState == 1 && (liveNeighbors == 2 || liveNeighbors == 3)) {
-			
-			} else if (tileState == 1 && liveNeighbors > 3) {
+				backup[col][row] = 1;
+			} else if (tileState == 1 && liveNeighbors >= 4) {
 				backup[col][row] = 0;
 			} else if (tileState == 0 && liveNeighbors == 3) {
 				backup[col][row] = 1;
@@ -81,6 +83,7 @@ function step() {
 	}
 	board = backup.slice();
 	render();
+	generation++;
 }
 // [col][row]
 function render() {
