2b64b96a5fe6d71fa46675d30b702019a4c461fc
Frank Paschen <fhp23@cs.drexel.edu>
2018-11-12 13:12:48 -0500

    i think it works

--- script.js
+++ script.js
@@ -11,14 +11,17 @@ var canvas;
 var ctx;
 var generation = 0;
 var speed = 1000;	// adjustable (ms)
-var scenario = 0;	// adjustable - todo: implement some scenarios from wikipedia page
+var scenario = 2;	// adjustable - todo: implement some scenarios from wikipedia page
 
 function init() {
 	console.log("hello");
 	board = new Array(boardSize);
-	//[row][column]
+
 	for (var i = 0; i < boardSize; i++) {
 		board[i] = new Array(boardSize);
+		for (var j = 0; j < boardSize; j++) {
+			board[i][j] = 0;
+		}
 	}
 	
 	canvas = document.getElementById('canvas');
@@ -26,19 +29,18 @@ function init() {
 	tileSize = ctx.canvas.width / boardSize;
 	
 	
-	if (scenario == 0) {	// random
-		for (var row = 0; row < boardSize; row++) {
-			for (var col = 0; col < boardSize; col++) {
+	if (scenario == 1) {	// random
+		for (var row = 25; row < 30; row++) {
+			for (var col = 25; col < 30; col++) {
 				board[row][col] = Math.round(Math.random());
 			}
 		}
+	} else if (scenario == 2) { // 10 cell row
+		for (var col = 25; col < 36; col++) {
+			board[col][col] = 1;
+		}
 	}
-
-	
-	
-	render();
-	step();
-	
+	setInterval(step, speed);
 }
 var backup;
 
@@ -46,8 +48,8 @@ var backup;
 function step() {
 	backup = board.slice();	// copy by value lol
 	// iterate through the game board. 0=dead, 1=alive
-	for (var col = 10; col < boardSize; col++) {
-		for (var row = 10; row < boardSize; row++) {
+	for (var col = 0; col < boardSize; col++) {
+		for (var row = 0; row < boardSize; row++) {
 			// implement rules of sim
 			var liveNeighbors = 0;	// count live # of neighbors
 			var tileState = board[col][row];
@@ -61,19 +63,33 @@ function step() {
 					liveNeighbors += (board[col+i][row-j]) ;
 				}
 			}
-			if (board[col][row] == 1) liveNeighbors -= 1;
-			console.log("board[" + col + "][" + row + "]:	" + liveNeighbors);		
+			if (board[col][row] == 1) liveNeighbors -= 1;	// ridiculous
+			
+			
+			/* implementing game rules */
+			
+			if (tileState == 1 && liveNeighbors < 2) {
+				backup[col][row] = 0;
+			} else if (tileState == 1 && (liveNeighbors == 2 || liveNeighbors == 3)) {
+			
+			} else if (tileState == 1 && liveNeighbors > 3) {
+				backup[col][row] = 0;
+			} else if (tileState == 0 && liveNeighbors == 3) {
+				backup[col][row] = 1;
+			} 
 		}
 	}
+	board = backup.slice();
+	render();
 }
 // [col][row]
 function render() {
 	if (canvas.getContext) {
 		for (var row = 0; row < boardSize; row++) {
 			for (var col = 0; col < boardSize; col++) {
-				if (board[row][col] == 0) {
+				if (board[col][row] == 0) {
 					ctx.fillStyle = 'orange';
-				} else if (board[row][col] == 1) {
+				} else if (board[col][row] == 1) {
 					ctx.fillStyle = 'green';
 				} else {
 					ctx.fillStyle = 'black';
