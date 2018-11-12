4402e1bd2385e1b2557532429b47c96ce669b83b
Frank Paschen <frankpaschen99@gmail.com>
2018-11-12 16:21:26 -0500

    counting neighbors works. almost there!

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-/*
-	This doesnt work properly. Gotta be an issue
-	counting live neighbors because the rules look
-	good.
-	
-	will fix later
-
-*/
-
 var board;
 var tileSize;
 var boardSize = 50; // adjustable
 var canvas;
 var ctx;
 var generation = 0;
-var speed = 1000;	// adjustable (ms)
-var scenario = 1;	// adjustable - todo: implement some scenarios from wikipedia page
+var speed = 50;	// adjustable (ms)
+var scenario = 2;	// adjustable - todo: implement some scenarios from wikipedia page
 
 function init() {
 	board = new Array(boardSize);
@@ -30,74 +21,66 @@ function init() {
 	tileSize = ctx.canvas.width / boardSize;
 	
 	if (scenario == 1) {	// random
-		for (var row = 0; row < boardSize; row++) {
-			for (var col = 0; col < boardSize; col++) {
+		for (var row = 1; row < boardSize-1; row++) {
+			for (var col = 1; col < boardSize-1; col++) {
 				board[row][col] = Math.round(Math.random());
 			}
 		}
 	} else if (scenario == 2) { // 10 cell row
-		for (var row = 25; row < 36; row++) {
+		for (var row = 25; row < 35; row++) {
 			board[row][25] = 1;
 		}
 	}
 	
 	render();
-	step();
-	//setInterval(step, speed);
+	setInterval(step, speed);
 }
 var backup;
 
-// called every generation 
 function step() {
+	
+	/****
+		Final issue: original board seems to be getting updated instead of the temp backup board.
+	****/
 	backup = board.slice();
 
-	for (var col = 0; col < boardSize; col++) {
-		for (var row = 0; row < boardSize; row++) {
+	// Start at 1 and end at boardSize-1 to avoid undefined border problems
+	for (var col = 1; col < boardSize-1; col++) {
+		for (var row = 1; row < boardSize-1; row++) {
 
 			var liveNeighbors = 0;
-			var tileState = board[col][row];
+			var tileState = board[row][col];
 
-			// Count live neighbors, skipping undefined tiles
+			/* Count live neighbors */
 			for (var i = -1; i < 2; i++) {
 				if (i == 0) continue;
 
-				// This is SO CLOSE to working. Does not work for the first 50 tiles or the last
-				// 50 tiles. (Top row and bottom row)
-
-				console.log("Calculating for: board[" + row + "][" + col + "]");
-				
 				liveNeighbors += board[row][col+i];
 				liveNeighbors += board[row+i][col];
 				liveNeighbors += board[row+i][col+i];
 				liveNeighbors += board[row+i][col-i];
 			}
-			
-			
-			
-			console.log("board[" + row + "][" + col + "]:	" + liveNeighbors);	
-			
-			/* implementing game rules */
-			
-			if (tileState == 1 && liveNeighbors < 2) {
-				backup[col][row] = 0;
-			} else if (tileState == 1 && (liveNeighbors == 2 || liveNeighbors == 3)) {
-				backup[col][row] = 1;
-			} else if (tileState == 1 && liveNeighbors >= 4) {
-				backup[col][row] = 0;
-			} else if (tileState == 0 && liveNeighbors == 3) {
-				backup[col][row] = 1;
-			} 
+
+			/* Implement game rules from wikipedia */
+			if (tileState == 1 && liveNeighbors < 2)
+				backup[row][col] = 0;
+			else if (tileState == 1 && (liveNeighbors == 2 || liveNeighbors == 3) )
+				backup[row][col] = 1;
+			else if (tileState == 1 && liveNeighbors > 3)
+				backup[row][col] = 0;
+			else if (tileState == 0 && liveNeighbors == 3)
+				backup[row][col] = 1;
 		}
 	}
 	board = backup.slice();
-	//render();
+	render();
 	generation++;
 }
-// [row][col]
+
 function render() {
 	if (canvas.getContext) {
-		for (var row = 0; row < boardSize; row++) {
-			for (var col = 0; col < boardSize; col++) {
+		for (var row = 1; row < boardSize-1; row++) {
+			for (var col = 1; col < boardSize-1; col++) {
 				if (board[row][col] == 0) {
 					ctx.fillStyle = 'orange';
 				} else if (board[row][col] == 1) {
